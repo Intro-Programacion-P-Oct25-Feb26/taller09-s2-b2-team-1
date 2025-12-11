8450cc0e57291c97b6664b1a5c47b411e2842040
alex bravp <fabriciobravo373@gmail.com>
2025-12-11 13:43:44 -0500

    ejercicio 4

--- EjerciciosT9/src/paquete02/Ejercicio04.java
+++ EjerciciosT9/src/paquete02/Ejercicio04.java
@@ -20,10 +20,26 @@ public class Ejercicio04 {
         String[] estudiantes = {"Kimberly Gonzalez", "Mark Hogan",
             "Teresa Martinez", "Julia Johnson", "Mark Cook", "Jennifer Manning",
             "Juan Vasquez"};
-
         String[] promediosCualitativos = new String[promedios.length];
 
         for (int i = 0; i < promedios.length; i++) {
             double promedio = promedios[i];
 
-           
+            if (promedio >= 9 && promedio <= 10) {
+                promediosCualitativos[i] = "Sobresaliente";
+            } else if (promedio >= 6 && promedio <= 8.9) {
+                promediosCualitativos[i] = "Bueno";
+            } else if (promedio >= 0 && promedio <= 5.9) {
+                promediosCualitativos[i] = "Regular";
+            }
+        }
+        for (int i = 0; i < promedios.length; i++) {
+            String promedioFormateado = String.format("%.2f", promedios[i]);
+            System.out.printf("%s promedio: %s promedio cualitativo %s\n",
+                    estudiantes[i],
+                    promedioFormateado,
+                    promediosCualitativos[i]);
+        }
+        
+    }
+}
