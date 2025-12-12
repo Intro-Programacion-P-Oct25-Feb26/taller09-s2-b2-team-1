4326cabc716e972c6e99023e7366df2d47b26d7c
GIT-gezzy <jdavichomaldonado@gmail.com>
2025-12-11 21:59:38 -0500

    Ejercicio07

--- EjerciciosT9/src/paquete02/Ejercicio07.java
+++ EjerciciosT9/src/paquete02/Ejercicio07.java
@@ -17,8 +17,23 @@ public class Ejercicio07 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      
-
+        Scanner entrada = new Scanner(System.in);
+        double[] numeros = new double[10];
+        double suma = 0;
+        String reporte = "";
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.printf("Ingrese el número %d: ", i + 1);
+            numeros[i] = entrada.nextDouble();
+            suma = suma + numeros[i];
+        }
+        double promedio = suma / numeros.length;
+        reporte = String.format("%sPromedio de los numero ingresados: %.2f\n",
+                reporte, promedio);
+        for (int i = 0; i < numeros.length; i++) {
+            double diferencia = numeros[i] - promedio;
+            reporte = String.format("%sNúmero %d: %.2f | Diferencia: %.2f\n",
+                    reporte, i + 1, numeros[i], diferencia);
+        }
+        System.out.printf("%s\n", reporte);
     }
-
 }
